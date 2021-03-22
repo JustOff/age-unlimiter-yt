7472dcd136216af5067a8bc719c39ca3fafe1399
JustOff <Off.Just.Off@gmail.com>
2021-03-22 20:19:01 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,9 @@
+@echo off
+set VER=2.0.1
+
+sed -i -E "s/version>.+?</version>%VER%</" install.rdf
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/age-unlimiter-yt-.+?\.xpi/download\/%VER%\/age-unlimiter-yt-%VER%\.xpi/" update.xml
+
+set XPI=age-unlimiter-yt-%VER%.xpi
+if exist %XPI% del %XPI%
+zip -r9q %XPI% * -x .git/* .gitignore update.xml LICENSE README.md *.cmd *.xpi *.exe
